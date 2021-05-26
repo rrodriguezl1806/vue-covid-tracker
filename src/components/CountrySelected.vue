4e73b74b0552ca5925c662c4621098d18153819b
Reynaldo R.L <rrodriguezl.1806@gmail.com>
2021-05-26 15:56:19 -0400

    update

---
+++ src/components/CountrySelected.vue
@@ -0,0 +1,28 @@
+<template>
+  <select
+      id="country"
+      class="form-select mt-10 block w-full border p-3 rounded"
+      v-model="selected"
+      @change="onChange()"
+  >
+    <option value="0">Select Country</option>
+    <option :value="country" v-for="country in countries">{{country.Country}}</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'CountrySelected',
+  props: ['countries'],
+  data() {
+    return {
+      selected: 0
+    }
+  },
+  methods: {
+    onChange() {
+      this.$emit('get-country', this.selected)
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate" />
+    <DataBoxes :stats="stats" />
+    <CountrySelected @get-country="getCountryData" :countries="countries"/>
+  </main>
+
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">
+      <i class="fa fa-spinner fa-spin mr-2" aria-hidden="true"></i>
+      Fetching Data
+    </div>
+  </main>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import DataTitle from "../components/DataTitle";
+import DataBoxes from "../components/DataBoxes";
+import CountrySelected from "../components/CountrySelected";
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    DataTitle,
+    DataBoxes,
+    CountrySelected
+  },
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: [],
+      loadingImage: false
+    }
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch('https://api.covid19api.com/summary')
+      return res.json()
+    },
+    getCountryData(country) {
+      this.stats = country
+      this.title = country.Country
+    }
+  },
+  async created() {
+    const data = await this.fetchCovidData()
+    this.dataDate = data.Date
+    this.stats = data.Global
+    this.countries = data.Countries
+    this.loading = false
+
   }
 }
 </script>
